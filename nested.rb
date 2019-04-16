61aa88b1795e350db0dd5db3853799b93fa2ab5f
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-16 02:49:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,6 @@ def adding_matz
   programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
   
   programmer_hash
-
-    
 end
 
 def changing_alan
@@ -105,10 +103,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
